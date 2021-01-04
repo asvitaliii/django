3157d9c82e28480c97d98fe91c6f88cb70201fd8
asvitaliii <a.s.vitaliii@gmail.com>
2021-01-04 10:59:53 +0200

    str changed)

--- StartPage/models.py
+++ StartPage/models.py
@@ -5,8 +5,14 @@ class Author(models.Model):
     name = models.CharField(max_length=25)
     about = models.TextField(max_length=2500)
 
+    def __str__(self):
+        return self.name
+
 
 class Verse(models.Model):
     name = models.CharField(max_length=25)
     text = models.TextField(max_length=2500)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}-{self.author}'
